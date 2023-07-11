aded12aaa0dce1cad0f9baf5ec6bcd050fd68056
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-11 16:09:54 -0300

    erros lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,14 +1,14 @@
 const data = require('../data/zoo_data');
- 
-  const getSpeciesByIds = (...ids) => {
-    if (ids.length === []) {
-      return ids;
-    }
-    return data.species.filter((species) => {
-       return ids.includes(species.id);
-    });  
-  };
+
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === []) {
+  return ids;
+  }
+  return data.species.filter((species) => {
+  return ids.includes(species.id);
+  });
+};
   
-  //console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 module.exports = getSpeciesByIds;
