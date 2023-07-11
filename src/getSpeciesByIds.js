d9611732d2c1d475d1cfe5da49d8ec02ef354daf
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-11 16:22:03 -0300

    lint.3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,9 +4,7 @@ const getSpeciesByIds = (...ids) => {
   if (ids) {
     return data.species.filter((species) => ids.includes(species.id));
   }
-  else {
     return [];
-  }
 };
 
 // console.log(getSpeciesByIds('0938aa3-f153-4937-9f88-4858b24d6bce'))
