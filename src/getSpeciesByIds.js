b17cda68cc4100b0f321c719fc33018cf83a2e0f
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-11 07:53:59 -0300

    1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (ids) => {
   // seu código aqui
+  data.species.forEach(element => {
+    if (element.id === ids) {
+      return element.name;
+    }
+    if (element.id.length === [0]) {
+      return [];
+    }
+  });
 };
 
 module.exports = getSpeciesByIds;
