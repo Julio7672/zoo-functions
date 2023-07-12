0047a165f74910df5890c69b248ad4b9a26a9eed
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-12 14:23:40 -0300

    3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,6 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
+  const pessoa = data.employees
+  .find((pessoa) => pessoa.firstName === employeeName || pessoa.lastName === employeeName)
+  if (pessoa) {
+    return pessoa
+  } 
+  return {};
   // seu código aqui
 };
 
