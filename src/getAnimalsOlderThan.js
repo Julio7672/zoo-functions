1a6325ad4cd35a517b6ebe0cc2c28772a4a09c08
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-12 13:03:23 -0300

    2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const especies = data.species.find((element, index) => element.name === animal);
+
+  if(especies.residents.every((animais) => animais.age >= age)) {
+        return true
+  } 
+    return false
 };
 
 module.exports = getAnimalsOlderThan;
