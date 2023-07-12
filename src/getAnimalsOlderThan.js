04ff069003b8787aef166d5297a2e6f991b178b8
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-12 13:06:56 -0300

    lint requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,10 +3,10 @@ const data = require('../data/zoo_data');
 const getAnimalsOlderThan = (animal, age) => {
   const especies = data.species.find((element, index) => element.name === animal);
 
-  if(especies.residents.every((animais) => animais.age >= age)) {
-        return true
-  } 
-    return false
+  if (especies.residents.every((animais) => animais.age >= age)) {
+    return true;
+  }
+  return false;
 };
 
 module.exports = getAnimalsOlderThan;
