30fee62d215d479fafc72fd5a66caca99e206f31
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-14 08:23:43 -0300

    requisito 5 lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,47 +1,44 @@
 const data = require('../data/zoo_data');
 
-const countAnimals_ = () => {
+const countAnimalsvazio = () => {
   const animalsArray = {};
   data.species.forEach((element) => {
     animalsArray[element.name] = element.residents.length;
-})
-return animalsArray;
-}
+  });
+  return animalsArray;
+};
 const countAnimalss = (animal) => {
-        const animalArray = []
-        if(Object.keys(animal).includes('species')) {
-       data.species.forEach((element) => {
-       const animalNum = element.residents.length        
-        if (element.name === animal.species) {
-          return animalArray.push(animalNum)
-        }     
-  })
-}
-  return animalArray[0]
-}
- 
+  const animalArray = [];
+  if(Object.keys(animal).includes('species')) {
+    data.species.forEach((element) => {
+    const animalNum = element.residents.length;      
+      if (element.name === animal.species) {
+        return animalArray.push(animalNum);
+      }  
+    });
+  }
+  return animalArray[0];
+};
 const countAnimalsSex = (animal) => {
-  const animalArray = []
-  if(Object.keys(animal).includes('species' && 'sex')) {
- data.species.forEach((element) => {        
-  if (element.name === animal.species) {
-    const animalNum = element.residents.filter((element) => element.sex === animal.sex).length
-    return animalArray.push(animalNum)
-  }     
-})
-}
-return animalArray[0]
-}
-
+  const animalArray = [];
+  if (Object.keys(animal).includes('species' && 'sex')) {
+    data.species.forEach((element) => {      
+      if (element.name === animal.species) {
+        const animalNum = element.residents.filter((elemento) => elemento.sex === animal.sex).length;
+        return animalArray.push(animalNum);
+      } 
+    });
+  }
+  return animalArray[0];
+};
 const countAnimals = (animal) => {
   if (animal === undefined) {
-    return countAnimals_();
+    return countAnimalsvazio();
   }
   if (Object.keys(animal).includes('sex')) {
-     return countAnimalsSex(animal)
+    return countAnimalsSex(animal);
   }
-  return countAnimalss(animal)
-  
+  return countAnimalss(animal);
 };
-//console.log(countAnimals({ species: 'penguins', sex: 'female' }))
+// console.log(countAnimals({ species: 'penguins', sex: 'female' }))
 module.exports = countAnimals;
