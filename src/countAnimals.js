eb2189228b82c925bdcf64d127abb18744aae8ef
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-14 08:07:07 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,47 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
-};
+const countAnimals_ = () => {
+  const animalsArray = {};
+  data.species.forEach((element) => {
+    animalsArray[element.name] = element.residents.length;
+})
+return animalsArray;
+}
+const countAnimalss = (animal) => {
+        const animalArray = []
+        if(Object.keys(animal).includes('species')) {
+       data.species.forEach((element) => {
+       const animalNum = element.residents.length        
+        if (element.name === animal.species) {
+          return animalArray.push(animalNum)
+        }     
+  })
+}
+  return animalArray[0]
+}
+ 
+const countAnimalsSex = (animal) => {
+  const animalArray = []
+  if(Object.keys(animal).includes('species' && 'sex')) {
+ data.species.forEach((element) => {        
+  if (element.name === animal.species) {
+    const animalNum = element.residents.filter((element) => element.sex === animal.sex).length
+    return animalArray.push(animalNum)
+  }     
+})
+}
+return animalArray[0]
+}
 
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    return countAnimals_();
+  }
+  if (Object.keys(animal).includes('sex')) {
+     return countAnimalsSex(animal)
+  }
+  return countAnimalss(animal)
+  
+};
+//console.log(countAnimals({ species: 'penguins', sex: 'female' }))
 module.exports = countAnimals;
