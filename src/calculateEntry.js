c0c7f579b68c2c8ab7209d6603b403189d6c6b0d
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-15 16:39:58 -0300

    erros.3

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,11 +4,13 @@ const countEntrants = (entrants) => {
   const obj = { child: 0, adult: 0, senior: 0 };
   entrants.map((pessoa) => {
     if (pessoa.age < 18) {
-      return obj.child += 1;
-    } else if (pessoa.age >= 18 && pessoa.age < 50) {
-      return obj.adult += 1;
-    } else if (pessoa.age >= 50) {
-      return obj.senior += 1;
+      obj.child += 1;
+    }
+    if (pessoa.age >= 18 && pessoa.age < 50) {
+      obj.adult += 1;
+    }
+    if (pessoa.age >= 50) {
+      obj.senior += 1;
     }
     return obj;
   });
@@ -16,17 +18,17 @@ const countEntrants = (entrants) => {
 };
 const calculateEntry = (entrants) => {
   if (!entrants) {
-    return 0
+    return 0;
   }
   if (entrants.length === 0) {
-    return 0
+    return 0;
   }
   const childPrice = data.prices.child;
   const adultPrice = data.prices.adult;
   const seniorPrice = data.prices.senior;
   if (countEntrants(entrants)) {
-    return (countEntrants(entrants).child * childPrice) 
-    + (countEntrants(entrants).adult * adultPrice) 
+    return (countEntrants(entrants).child * childPrice)
+    + (countEntrants(entrants).adult * adultPrice)
     + (countEntrants(entrants).senior * seniorPrice);
   }
 };
