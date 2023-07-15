30ce23450e8d9030c3fb049da359d7b2b8432572
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-15 20:55:56 -0300

    12-13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,35 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+    it('', () => {
+        expect(getOpeningHours('saturday', '9:56-am')).toEqual('The zoo is open');
+    });
+    it('', () => {
+        expect(getOpeningHours('sunday', '9:56-pm')).toEqual('The zoo is closed');
+    });
+    it('', () => {
+        expect(() => {
+            getOpeningHours('sunday', '9:560-pm');
+        }).toThrow('The minutes must be between 0 and 59');
+    });
+    it('', () => {
+        expect(() => {
+            getOpeningHours('sunday', '90:56-pm');
+        }).toThrow('The hour must be between 0 and 12');
+    });
+    it('', () => {
+        expect(() => {
+            getOpeningHours('domingo', '9:56-pm');
+        }).toThrow('The day must be valid. Example: Monday');
+    });
+    it('', () => {
+        expect(() => {
+            getOpeningHours('sunday', '9:56-');
+        }).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+    });
+    it('', () => {
+        expect(() => {
+            getOpeningHours('sunday', '#');
+        }).toThrow(`The hour should represent a number`);
+    });
+});
