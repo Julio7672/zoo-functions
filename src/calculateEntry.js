8d889e68427991cad8142aa7ca712a6f94f84df9
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-15 16:28:14 -0300

    erros.2

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,17 +2,12 @@ const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
   const obj = { child: 0, adult: 0, senior: 0 };
-  if (entrants === undefined) {
-    return 0;
-  }
   entrants.map((pessoa) => {
     if (pessoa.age < 18) {
       return obj.child += 1;
-    }
-    else if (pessoa.age >= 18 && pessoa.age < 50) {
+    } else if (pessoa.age >= 18 && pessoa.age < 50) {
       return obj.adult += 1;
-    }
-    else if (pessoa.age >= 50) {
+    } else if (pessoa.age >= 50) {
       return obj.senior += 1;
     }
     return obj;
@@ -20,6 +15,12 @@ const countEntrants = (entrants) => {
   return obj;
 };
 const calculateEntry = (entrants) => {
+  if (!entrants) {
+    return 0
+  }
+  if (entrants.length === 0) {
+    return 0
+  }
   const childPrice = data.prices.child;
   const adultPrice = data.prices.adult;
   const seniorPrice = data.prices.senior;
@@ -28,7 +29,6 @@ const calculateEntry = (entrants) => {
     + (countEntrants(entrants).adult * adultPrice) 
     + (countEntrants(entrants).senior * seniorPrice);
   }
-  return 0;
 };
 
 module.exports = { calculateEntry, countEntrants };
