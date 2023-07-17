01e923c49fd93fd252ecbbd56dc72eb575ae54b8
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-17 00:27:45 -0300

    lint erros

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,13 +4,13 @@ const getOldestFromFirstSpecies = (id) => {
   const arrayDeAnimais = (data.employees.find((element) => element.id === id).responsibleFor);
   const firstAnimal = arrayDeAnimais[0];
   const animal = data.species.find((elemento) => elemento.id === firstAnimal);
-  const residents = animal.residents.filter((elementu) => elementu.age).map((ele)=> {
-     return ele.age
-  })
-  const ageOfOldestAnimal = residents.sort((a,b) => b - a )[0];
-    return Object.values(animal.residents.find((oldestAnimal) => oldestAnimal.age === ageOfOldestAnimal));
-   
-}
+  const residents = animal.residents.filter((elementu) => elementu.age).map((ele) => {
+     return ele.age;
+  });
+  const ageOfOldestAnimal = residents.sort((a, b) => b - a)[0];
+  return Object.values(animal.residents
+    .find((oldestAnimal) => oldestAnimal.age === ageOfOldestAnimal));
+};
 // console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'))
 
 module.exports = getOldestFromFirstSpecies;
