d56a7fca684eb06eff2c1566d5607e991b97a8ef
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-18 12:36:29 -0300

    lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,67 +1,65 @@
 const data = require('../data/zoo_data');
 const getScheduleDay = (scheduleTarget) => {
   if (scheduleTarget === 'Monday') {
-    const obj0 = {}
+    const obj0 = {};
     obj0[scheduleTarget] = {
       officeHour: 'CLOSED',
       exhibition: 'The zoo will be closed!',
-    }
-    return obj0
+    };
+    return obj0;
   }
-   const array = []
-   const Obj = {}
-   const animaisDay =  data.species.filter((element) => element.availability.includes(scheduleTarget))
+   const array = [];
+   const Obj = {};
+   const animaisDay =  data.species.filter((element) => element.availability.includes(scheduleTarget));
     animaisDay.forEach((element)=> {
-     array.push(element.name)
+     array.push(element.name);
    })
-   const hourOpen = data.hours[scheduleTarget].open
-   const hourClose = data.hours[scheduleTarget].close
+   const hourOpen = data.hours[scheduleTarget].open;
+   const hourClose = data.hours[scheduleTarget].close;
     Obj[scheduleTarget] = {
     officeHour: `Open from ${hourOpen}am until ${hourClose}pm`,
-    exhibition: array
+    exhibition: array,
    }
-    return Obj
+    return Obj;
 };
 
 const getScheduleNoParameter = (scheduleTarget) => {
-  const obj = {}
+  const obj = {};
   const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  weekDays.forEach((elemento)=>{
-    const array = []
-    const arrayAnimaisPerDay = data.species.filter((element) => element.availability.includes(elemento))
-  //const array = arrayAnimaisPerDay.reduce((acc, cur) => [...acc, cur.name], [])
-  arrayAnimaisPerDay.forEach((ele)=>{
-    array.push(ele.name)
+  weekDays.forEach((elemento) => {
+    const array = [];
+    const arrayAnimaisPerDay = data.species.filter((element) => element.availability.includes(elemento)) // const array = arrayAnimaisPerDay.reduce((acc, cur) => [...acc, cur.name], [])
+  arrayAnimaisPerDay.forEach((ele) => {
+    array.push(ele.name);
   })
-
     obj[elemento] = {
       officeHour: `Open from ${data.hours[elemento].open}am until ${data.hours[elemento].close}pm`,
-      exhibition: array
-     }
+      exhibition: array,
+     };
      if (elemento === 'Monday') {
        obj['Monday'] = {
          officeHour: 'CLOSED',
          exhibition: 'The zoo will be closed!',
-       }
+       };
      }
    })
-  return obj
+  return obj;
 };
 const getScheduleAnimal = (scheduleTarget) => {
-    return data.species.find((element) => element.name === scheduleTarget).availability
+    return data.species.find((element) => element.name === scheduleTarget).availability;
 };
 const getSchedule = (scheduleTarget) => {
   const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
- const array = []
-  data.species.forEach((element)=> array.push(element.name))
+ const array = [];
+  data.species.forEach((element)=> array.push(element.name));
   if (!scheduleTarget || (weekDays.includes(scheduleTarget) === false && array.includes(scheduleTarget) === false)) {
-    return getScheduleNoParameter()
+    return getScheduleNoParameter();
   }
   if (weekDays.some((element) => element === scheduleTarget)) {
-    return getScheduleDay(scheduleTarget)
+    return getScheduleDay(scheduleTarget);
  }
   if (data.species.filter((element) => element.name === scheduleTarget)) {
-    return getScheduleAnimal(scheduleTarget)
+    return getScheduleAnimal(scheduleTarget);
   }
 };
 console.log(getSchedule())
