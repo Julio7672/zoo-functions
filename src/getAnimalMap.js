334cc77b2f44b0727542eefc6748a8b48c12ec92
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-20 16:03:20 -0300

    lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,20 +2,17 @@ const data = require('../data/zoo_data');
 
 const getAnimalMapNoParameter = () => {
   const obj = {};
-    data.species.forEach((element) => {
-      const array = [];
-       data.species.filter((elemento) => element.location.includes(elemento.location))
-       .forEach((elementi) => {
-        array.push(elementi.name);
-       });
-       return obj[element.location] = array;
-    }) 
+  data.species.forEach((element) => {
+    const array = [];
+    data.species.filter((elemento) => element.location.includes(elemento.location))
+   .forEach((elementi) => {
+      array.push(elementi.name);
+    });
+    return obj[element.location] = array;
+  });
   return obj;
 };
-const getAnimalMap = (options) => {
-  if (!options || !options.includeNames) {
-    return getAnimalMapNoParameter();
-  }
+const getAnimalMapSexSorted = (options) => {
   const obj0 = {};
   data.species.forEach((ele) => obj0[ele.location] = []);
   data.species.forEach((elemento) => {
@@ -26,11 +23,17 @@ const getAnimalMap = (options) => {
       if (options.sorted) {
         arrayNames.sort();
       }
-      return obj0[elemento.location].push({ [elemento.name]: arrayNames });
-    } 
+      obj0[elemento.location].push({ [elemento.name]: arrayNames });
+    }
   });
   return obj0;
 };
+const getAnimalMap = (options) => {
+  if (!options || !options.includeNames) {
+    return getAnimalMapNoParameter();
+  }
+  return getAnimalMapSexSorted(options);
+};
 
- console.log(getAnimalMap({includeNames: true, sex: 'male', sorted: true}))
+// console.log(getAnimalMap())
 module.exports = getAnimalMap;
