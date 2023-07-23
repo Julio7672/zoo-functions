468aa37f5aed3a1254ec6b1ff0a199974bb1525d
Julio Alexandre <julioalexandre499@gmail.com>
2023-07-23 16:42:39 -0300

    lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,47 +1,46 @@
 const data = require('../data/zoo_data');
 
 const getEmployeesCoverageAnimalNames = (ids) => {
-  const array = []
- data.species.filter((element) => ids.includes(element.id)).forEach((ele) => {
-  array.push(ele.name)
- })
- return array
+  const array = [];
+  data.species.filter((element) => ids.includes(element.id)).forEach((ele) => {
+    array.push(ele.name);
+  });
+  return array;
 };
 
 const getEmployeesCoverageAnimallocations = (ids) => {
-  const array = []
- data.species.filter((element) => ids.includes(element.id)).forEach((ele) => {
-  array.push(ele.location)
- })
- return array
+  const array = [];
+  data.species.filter((element) => ids.includes(element.id)).forEach((ele) => {
+    array.push(ele.location);
+  });
+  return array;
 };
 
-const getEmployeesCoverageNoParameter = () => {
-  return data.employees.map((ele) => {
+const getEmployeesCoverageNoParameter = () => data.employees.map((ele) => {
     const obj = {
-    id: ele.id,
-    fullName:`${ele.firstName} ${ele.lastName}`,
-    species: getEmployeesCoverageAnimalNames(ele.responsibleFor),
-    locations: getEmployeesCoverageAnimallocations(ele.responsibleFor),
-  }
-  return obj
-  })
-};
+      id: ele.id,
+      fullName: `${ele.firstName} ${ele.lastName}`,
+      species: getEmployeesCoverageAnimalNames(ele.responsibleFor),
+      locations: getEmployeesCoverageAnimallocations(ele.responsibleFor),
+    };
+    return obj;
+  });
 
 const getEmployeesCoverage = (infos) => {
   if (!infos) {
-    return getEmployeesCoverageNoParameter()
+    return getEmployeesCoverageNoParameter();
   }
-  const obj = {}
-  const empregado = data.employees.find((element) =>  infos.name === element.firstName || infos.name === element.lastName || infos.id === element.id)
+  const obj = {};
+  const empregado = data.employees.find((element) => infos.name === element.firstName
+   || infos.name === element.lastName || infos.id === element.id);
   if (!empregado) {
-    throw new Error('Informações inválidas')
+    throw new Error('Informações inválidas');
   }
-  obj.id = empregado.id
-  obj.fullName = `${empregado.firstName} ${empregado.lastName}`
-  obj.species = getEmployeesCoverageAnimalNames(empregado.responsibleFor)
-  obj.locations = getEmployeesCoverageAnimallocations(empregado.responsibleFor)
-  return obj
+  obj.id = empregado.id;
+  obj.fullName = `${empregado.firstName} ${empregado.lastName}`;
+  obj.species = getEmployeesCoverageAnimalNames(empregado.responsibleFor);
+  obj.locations = getEmployeesCoverageAnimallocations(empregado.responsibleFor);
+  return obj;
 };
-console.log(getEmployeesCoverageNoParameter())
+console.log(getEmployeesCoverageNoParameter());
 module.exports = getEmployeesCoverage;
